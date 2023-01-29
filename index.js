439c89f1fdb6a31677bbd61b1e178eaf31146430
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-01-29 19:31:01 +0530

    basic done

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node.right !== null) {
         prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
     }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
     if (node.left !== null) {
         prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
