a9656b2983e74c076d5d5fedc733ed8a1a2685b4
Rohit Mehta <63762981+r0hitm@users.noreply.github.com>
2023-02-08 18:10:09 +0530

    WIp

--- knightMoves.js
+++ knightMoves.js
@@ -12,29 +12,39 @@ const assert = require("assert");
 // where x and y are the coordinates of the chessboard
 // Note: 0 <= x, y <= 7
 
-
 const knightMoves = (start, end) => {
-    // todo
+    return buildPath(start, end, [start]);
 };
-
 
 // Return the shortest path from start to end as an array of positions
 // Note: knightMoves helper. Not to be used directly
 const buildPath = (start, end, visited = []) => {
+    // DEBUG CODE ----------------
+    console.log("23: Entering buildPath: ", {start, end, visited});
+    // --------------------
+
     if (equals(start, end)) {
         return visited;
     } else {
-        const moves = getNextPos(start);
-        console.log(moves);
+        const moves = getNextPos(start).filter(pos => {
+            return visited.every(visitedPos => {
+                return equals(pos, visitedPos) === false;
+            });
+        });
+        // console.log("34: ", {start, moves});
+        // return;
         const shortestPath = moves.map(move => {
             return buildPath(move, end, visited.concat([move]));
         });
+        // console.log("39: ", shortestPath);
+        return;
         return shortestPath.reduce((a, b) => {
             return a.length < b.length ? a : b;
         });
     }
 };
 
+// DEBUG NOTE: This MIGHT not work
 // Get next possible positions from the given position
 const getNextPos = pos => {
     const [x, y] = pos;
@@ -48,19 +58,18 @@ const getNextPos = pos => {
         [x - 2, y + 1],
         [x - 2, y - 1],
     ].filter(p =>
-        p.every(
-            coord => coord >= 0 && coord <= 7 && equals(p, pos) === false
-        )
+        p.every(coord => coord >= 0 && coord <= 7 && equals(p, pos) === false)
     );
 };
 
+// DEBUG NOTE: This function WORKS
 // Returns true if this position is equal to the given position
 const equals = (pos1, pos2) => {
     return pos1[0] === pos2[0] && pos1[1] === pos2[1];
 };
 
 // console.log(buildPath([3, 3], [4, 3]));
-console.log(buildPath([0, 0], [1, 2]));
+console.log("72: ", buildPath([0, 0], [1, 2], [[0, 0]]));
 // console.log(buildPath([3, 3], [0, 0]));
 
 // console.log(knightMoves([3, 3], [4, 3]));
